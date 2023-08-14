8407f32ddc1e983abe2cd005c53c4de251e7dbe6
NF Tree <106249836+nftree721@users.noreply.github.com>
2023-08-14 15:55:18 +0200

    v1

---
+++ code/src/main/java/org/tj/hedera/samples/HelloStableCoin.java
@@ -0,0 +1,66 @@
+package org.tj.hedera.samples;
+
+import com.hedera.hashgraph.sdk.*;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.tj.hedera.samples.Utils.log;
+
+public class HelloStableCoin {
+    public static void main(String[] args) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
+        log("*** HelloStableCoin ***");
+
+        AccountId myAccountId = AccountId.fromString("0.0.467739");
+        log("AccountId: " + myAccountId);
+        PrivateKey myPrivateKey = PrivateKey.fromString("3030020100300706052b8104000a04220420daf20fec209b4da654c4dc3de5c0df2951fcc726a8d90f0cc127be278ba9a650");
+        log("Private Key: " + myPrivateKey);
+
+        Client client = Client.forTestnet();
+        client.setOperator(myAccountId, myPrivateKey);
+        client.setDefaultMaxTransactionFee(new Hbar(100));
+        client.setMaxQueryPayment(new Hbar(50));
+
+        //Request the cost of the query
+        //Check the new account's balance
+        AccountBalance accountBalanceNew = new AccountBalanceQuery()
+                .setAccountId(myAccountId)
+                .execute(client);
+
+        System.out.println("The new account balance is: " +accountBalanceNew.hbars);
+
+        // CREATE FUNGIBLE TOKEN (STABLECOIN)
+        final AccountId treasuryId = myAccountId;
+        log("CREATE FUNGIBLE TOKEN (STABLECOIN)");
+        TokenCreateTransaction tokenCreateTx = new TokenCreateTransaction()
+                .setTokenName("ZODIA USD")
+                .setTokenSymbol("ZUSD")
+                .setTokenType(TokenType.FUNGIBLE_COMMON)
+                .setDecimals(6)
+                .setInitialSupply(1_000_000)
+                .setTreasuryAccountId(treasuryId)
+                .setSupplyType(TokenSupplyType.INFINITE)
+                .setSupplyKey(myPrivateKey.getPublicKey())
+                .freezeWith(client);
+
+        //SIGN WITH TREASURY KEY
+        log("SIGN WITH TREASURY KEY");
+        TokenCreateTransaction tokenCreateSign = tokenCreateTx.sign(myPrivateKey);
+
+        //SUBMIT THE TRANSACTION
+        log("SUBMIT THE TRANSACTION");
+        TransactionResponse tokenCreateSubmit = tokenCreateSign.execute(client);
+
+        //GET THE TRANSACTION RECEIPT
+        log("GET THE TRANSACTION RECEIPT");
+        TransactionReceipt tokenCreateRx = tokenCreateSubmit.getReceipt(client);
+
+        //GET THE TOKEN ID
+        log("GET THE TOKEN ID");
+        TokenId tokenId = tokenCreateRx.tokenId;
+
+        //LOG THE TOKEN ID TO THE CONSOLE
+        System.out.println("Created token with ID: " +tokenId);
+        // Created token with ID: 0.0.477461
+        // https://testnet.dragonglass.me/hedera/search?q=0.0.477461
+    }
+}
---
+++ code/src/main/java/org/tj/hedera/samples/Utils.java
@@ -0,0 +1,7 @@
+package org.tj.hedera.samples;
+
+public class Utils {
+    public static void log(final Object o) {
+        System.out.println("" + o);
+    }
+}
